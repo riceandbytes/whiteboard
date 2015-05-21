1237aeaa49a864d8e40650bf03ab167ca2d8a5fe
vh <riceandbytes@gmail.com>
2015-05-20 23:13:22 -0700

    added line width and alpha story board

---
+++ whiteboard/src/SpecialPanelView.h
@@ -0,0 +1,13 @@
+//
+//  SpecialPanelView.h
+//  whiteboard
+//
+//  Created by visvavince on 5/20/15.
+//  Copyright (c) 2015 whiteboard. All rights reserved.
+//
+
+#import "PanelView.h"
+
+@interface SpecialPanelView : PanelView
+
+@end
---
+++ whiteboard/src/SpecialPanelView.m
@@ -0,0 +1,21 @@
+//
+//  SpecialPanelView.m
+//  whiteboard
+//
+//  Created by visvavince on 5/20/15.
+//  Copyright (c) 2015 whiteboard. All rights reserved.
+//
+
+#import "SpecialPanelView.h"
+
+@implementation SpecialPanelView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
